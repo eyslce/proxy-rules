acfabb071aaa1ba57fbb18685f7abaac936455c5
eyslce <eyslce@gmail.com>
2025-12-10 19:28:29 +0800

    update rule

--- clash-verge-rev.js
+++ clash-verge-rev.js
@@ -1,28 +1,55 @@
 // 你要插入的自定义规则（前置）
-const prependRule = [
-    "DOMAIN-SUFFIX,gemini.google.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,bard.google.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,aistudio.google.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,ai.google.dev,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,deepmind.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,deepmind.google.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,generativelanguage.googleapis.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,proactivebackend-pa.googleapis.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,alkalimakersuite-pa.clients6.google.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,fonts.googleapis.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,fonts.gstatic.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,gstatic.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,www.gstatic.com,🐟 漏网之鱼",
-    "DOMAIN-SUFFIX,googleusercontent.com,🐟 漏网之鱼"
+const prependRule = [    
+    "DOMAIN-SUFFIX,gemini.google.com,PROXY",
+    "DOMAIN-SUFFIX,bard.google.com,PROXY",
+    "DOMAIN-SUFFIX,aistudio.google.com,PROXY",
+    "DOMAIN-SUFFIX,ai.google.dev,PROXY",
+    "DOMAIN-SUFFIX,deepmind.com,PROXY",
+    "DOMAIN-SUFFIX,deepmind.google.com,PROXY",
+    "DOMAIN-SUFFIX,generativelanguage.googleapis.com,PROXY",
+    "DOMAIN-SUFFIX,proactivebackend-pa.googleapis.com,PROXY",
+    "DOMAIN-SUFFIX,alkalimakersuite-pa.clients6.google.com,PROXY",
+    "DOMAIN-SUFFIX,fonts.googleapis.com,PROXY",
+    "DOMAIN-SUFFIX,fonts.gstatic.com,PROXY",
+    "DOMAIN-SUFFIX,gstatic.com,PROXY",
+    "DOMAIN-SUFFIX,www.gstatic.com,PROXY",
+    "DOMAIN-SUFFIX,googleusercontent.com,PROXY",
+    "DOMAIN-SUFFIX,claude.ai,PROXY",
+    "DOMAIN-SUFFIX,anthropic.com,PROXY",
+    "DOMAIN-SUFFIX,claudeusercontent.com,PROXY",
+    "DOMAIN-SUFFIX,cdn.usefathom.com,PROXY",
+    "DOMAIN-SUFFIX,console.anthropic.com,PROXY",
+    "DOMAIN-SUFFIX,aicursor.com,PROXY",
+    "DOMAIN-SUFFIX,cursor.sh,PROXY",
+    "DOMAIN-SUFFIX,openai.com,PROXY",
+    "DOMAIN-SUFFIX,intercom.io,PROXY",
+    "DOMAIN-SUFFIX,intercomcdn.com,PROXY",
+    "DOMAIN-SUFFIX,cursor.so,PROXY"
   ];
 
 // 入口函数（Clash Verge Rev 自动调用）
 function main(config,profileName) {
+  rules = []
+   // profileName == EEVPN 时，将 PROXY 替换为 EEVPN
+   // profileName == miaoss 时，将 PROXY 替换为 🐟 漏网之鱼
+   if (profileName === "EEVPN") {
+    prependRule.forEach((rule) => {
+      rule = rule.replace("PROXY", "EEVPN");
+      rules.push(rule);
+    });
+  }
+   else if (profileName === "miaoss") {
+    prependRule.forEach((rule) => {
+      rule = rule.replace("PROXY", "🐟 漏网之鱼");
+      rules.push(rule);
+    });
+  }
+
     // 获取原 rules
     const oldRules = config.rules || [];
 
     // 插入到最前
-    config.rules = prependRule.concat(oldRules);
+    config.rules = rules.concat(oldRules);
 
     return config;
 }
